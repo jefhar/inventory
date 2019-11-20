f08fc2b0e3f6dcac8ca7d444545b5c1cb8c8cb60
Jeff Harris <jeff@c11k.com>
2019-11-19 22:48:47 -0800

    React Submits workorder create.

--- resources/js/components/WorkOrder/ClientCompanyName.js
+++ resources/js/components/WorkOrder/ClientCompanyName.js
@@ -3,28 +3,29 @@ import { AsyncTypeahead } from "react-bootstrap-typeahead";
 import "react-bootstrap-typeahead/css/Typeahead.css";
 
 function ClientCompanyName(props) {
-    return (
-        <AsyncTypeahead
-            allowNew={true}
-            autoFocus={true}
-            bsSize="sm"
-            id={props.id}
-            inputProps={{
-                className: "form-control form-control-sm",
-                name: "company_name",
-                required: "required"
-            }}
-            isLoading={props.isLoading}
-            labelKey={props.labelKey}
-            newSelectionPrefix={props.newSelectionPrefix}
-            onChange={props.handleChange}
-            onSearch={props.onSearch}
-            options={props.options}
-            placeholder={props.placeholder}
-            selected={props.selected}
-            selectHintOnEnter={true}
-        />
-    );
+  return (
+    <AsyncTypeahead
+      allowNew={true}
+      autoFocus={true}
+      bsSize="sm"
+      id={props.id}
+      inputProps={{
+        className: "form-control form-control-sm",
+        name: "company_name",
+        required: "required"
+      }}
+      isLoading={props.isLoading}
+      labelKey={props.labelKey}
+      newSelectionPrefix={props.newSelectionPrefix}
+      onBlur={props.onBlur}
+      onChange={props.handleChange}
+      onSearch={props.onSearch}
+      options={props.options}
+      placeholder={props.placeholder}
+      selected={props.selected}
+      selectHintOnEnter={true}
+    />
+  );
 }
 
 export default ClientCompanyName;
