ac3fb309335be236828adf82b5c9b5f12336da31
Jeff Harris <jeff@c11k.com>
2019-12-06 13:36:54 -0800

    Found unexpected outcome with assertSee().
    Views encode `'` to htmlentity. AssertSee() does not convert asserted `'`, so assertSee could use an `htmlspecialchars` inside. Also added person that is needed by Client.

--- tests/Feature/ClientsControllerTest.php
+++ tests/Feature/ClientsControllerTest.php
@@ -6,6 +6,7 @@ use App\Admin\Permissions\UserPermissions;
 use App\User;
 use App\WorkOrders\Controllers\ClientsController;
 use Domain\WorkOrders\Client;
+use Domain\WorkOrders\Person;
 use Tests\TestCase;
 
 class ClientsControllerTest extends TestCase
@@ -15,11 +16,15 @@ class ClientsControllerTest extends TestCase
      */
     public function clientsShowPageListsWorkOrders(): void
     {
+        $person = factory(Person::class)->make();
         $client = factory(Client::class)->create();
+        $client->person()->save($person);
+        $client->company_name = "O'" . $client->company_name;
+        $client->save();
         $user = factory(User::class)->create();
         $user->givePermissionTo(UserPermissions::IS_EMPLOYEE);
         $this->actingAs($user)
             ->get(route(ClientsController::SHOW_NAME, $client))
-            ->assertSee($client->company_name);
+            ->assertSee(htmlspecialchars($client->company_name, ENT_COMPAT | ENT_HTML401 | ENT_QUOTES));
     }
 }
