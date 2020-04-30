929ec138cd3125c34ca387399486efbc5cc5f0f1
Jeff Harris <jeff@jeffharris.us>
2020-04-29 23:03:11 -0700

    Login dusk test for salesRep

---
+++ tests/Browser/SalesRepTest.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Copyright 2018, 2019, 2020 Jeff Harris
+ * PHP Version 7.4
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Browser;
+
+use App\Admin\Permissions\UserRoles;
+use App\User;
+use Faker\Factory;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use Tests\Traits\FullUsers;
+
+class SalesRepTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+    use FullUsers;
+
+    /**
+     * @throws \Throwable
+     * @test
+     */
+    public function canLogin(): void
+    {
+        $faker = Factory::create();
+        $user = $this->createEmployee(UserRoles::SALES_REP);
+        $this->browse(
+            function (Browser $browser) use ($faker, $user) {
+                $browser->visit('/')
+                    ->click('Login')
+                    ->type('email', $user->email)
+                    ->type('password', 'password')
+                    ->press('Login')
+                    ->assertPathIs('/home');
+                $companyName = $faker->company;
+                $firstName = $faker->firstName;
+                $lastName = $faker->lastName;
+                $browser->press('Create New WorkOrder')
+                    ->type('company_name', $companyName)
+                    ->type('first_name', $firstName)
+                    ->type('last_name', $lastName)
+                    ->press('Create New Work Order')
+                    ->waitFor('#workorders_edit')
+                    ->assertSee($companyName)
+                    ->assertSee($firstName . ' ' . $lastName);
+            }
+        );
+    }
+}
