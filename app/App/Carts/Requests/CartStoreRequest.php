8561e165baa214b7e8c981b1d941c39885d424ee
Jeff Harris <jeff@jeffharris.us>
2020-05-29 22:39:19 -0700

    Sorted constants.

--- app/App/Carts/Requests/CartStoreRequest.php
+++ app/App/Carts/Requests/CartStoreRequest.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
 namespace App\Carts\Requests;
 
 use App\Carts\DataTransferObjects\CartStoreObject;
+use App\WorkOrders\DataTransferObjects\PersonObject;
 use Domain\Products\Models\Product;
 use Illuminate\Foundation\Http\FormRequest;
 
@@ -20,13 +21,14 @@ use Illuminate\Foundation\Http\FormRequest;
  */
 class CartStoreRequest extends FormRequest
 {
-    public const CLIENT_COMPANY_NAME = CartStoreObject::CLIENT_COMPANY_NAME;
-    public const PRODUCT_ID = CartStoreObject::PRODUCT_ID;
-
     private const RULES = [
         self::PRODUCT_ID => ['required', 'exists:' . Product::TABLE . ',' . Product::LUHN],
         self::CLIENT_COMPANY_NAME => ['required'],
     ];
+
+    public const CLIENT_COMPANY_NAME = CartStoreObject::CLIENT_COMPANY_NAME;
+    public const FIRST_NAME = PersonObject::FIRST_NAME;
+    public const PRODUCT_ID = CartStoreObject::PRODUCT_ID;
 
     /**
      * @return array
