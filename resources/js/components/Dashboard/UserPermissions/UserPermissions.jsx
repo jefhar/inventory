6bc838af19feea644dd266b04246df15670f203f
Jeff Harris <jeff@jeffharris.us>
2020-06-01 15:20:37 -0700

    Update UserRoles to use Button Group, duplicated into userPermissions.

--- resources/js/components/Dashboard/UserPermissions/UserPermissions.jsx
+++ resources/js/components/Dashboard/UserPermissions/UserPermissions.jsx
@@ -1,14 +1,64 @@
 import * as React from 'react'
+import PropTypes from 'prop-types'
+import { Button, ButtonGroup, Spinner } from 'reactstrap'
 
 class UserPermissions extends React.Component {
   constructor(props) {
     super(props)
     console.log('constructing.')
+    this.state = {
+      permissionsSelected: [],
+    }
+  }
+
+  setSelected(permission) {
+    console.info(`Chosen ${permission}.`)
+    const permissionsSelected = this.state.permissionsSelected
+    const index = permissionsSelected.indexOf(permission)
+    if (index < 0) {
+      permissionsSelected.push(permission)
+    } else {
+      permissionsSelected.splice(index, 1)
+    }
+    this.setState({ permissionsSelected: permissionsSelected })
   }
 
   render() {
-    return <></>
+    if (this.props.isLoaded) {
+      const permissions = JSON.parse(this.props.permissions)
+      const permissionItems = permissions.map((permission) => (
+        <Button
+          key={permission.id}
+          color="secondary"
+          onClick={() => this.setSelected(permission.id)}
+          active={this.state.permissionsSelected.includes(`${permission.id}`)}
+        >
+          {permission.name}
+        </Button>
+      ))
+      return (
+        <>
+          <p className="lead">Select Permissions:</p>
+          <ButtonGroup className="flex-wrap align-content-end">
+            {permissionItems}
+          </ButtonGroup>
+        </>
+      )
+    } else {
+      return (
+        <div className="row py-4 px-1 bg-gray-100">
+          <Spinner color="gray-300" type="grow" />
+          <span className="pl-2 text-black-50">
+            Loading User Permissions&hellip;
+          </span>
+        </div>
+      )
+    }
   }
 }
 
+UserPermissions.propTypes = {
+  isLoaded: PropTypes.bool,
+  permissions: PropTypes.string,
+}
 export default UserPermissions
--- resources/js/components/Dashboard/UserRoles/UserRoles.jsx
+++ resources/js/components/Dashboard/UserRoles/UserRoles.jsx
@@ -1,34 +1,50 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
-import { FormGroup, Input, Label, Spinner } from 'reactstrap'
+import { Button, ButtonGroup, Spinner } from 'reactstrap'
 
 class UserRoles extends React.Component {
   constructor(props) {
     super(props)
     console.log('constructing.')
+    this.setSelected = this.setSelected.bind(this)
+    this.state = {
+      roleSelected: null,
+    }
+  }
+
+  setSelected(role) {
+    console.info(`Chosen ${role}.`)
+    this.setState({ roleSelected: role })
   }
 
   render() {
     if (this.props.isLoaded) {
-      console.info('this.props.roles', this.props.roles)
       const roles = JSON.parse(this.props.roles)
-      console.info('roles', roles)
 
       const roleItems = roles.map((role) => (
-        <FormGroup check inline key={role.id}>
-          <Input type="radio" name="role" value={role.name} />
-          <Label check className="text-gray-100">
-            {role.name}
-          </Label>
-        </FormGroup>
+        <Button
+          key={role.id}
+          color="primary"
+          onClick={() => this.setSelected(role.id)}
+          active={this.state.roleSelected === `${role.id}`}
+        >
+          {role.name}
+        </Button>
       ))
 
-      return <div className="row py-4 px-2 bg-info">{roleItems}</div>
+      return (
+        <>
+          <p className="lead">Select a Role:</p>
+          <ButtonGroup>{roleItems}</ButtonGroup>
+        </>
+      )
     } else {
       return (
         <div className="row py-4 px-1 bg-gray-100">
           <Spinner color="gray-300" type="grow" />
-          <span className="pl-2 text-black-50">Loading User Roles&hellip;</span>
+          <span className="pl-2 text-black-50">
+            Select or create a user to assign a role&hellip;
+          </span>
         </div>
       )
     }
