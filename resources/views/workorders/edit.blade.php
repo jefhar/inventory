450c13db4167bbaaeb974d410886537e836d6b49
Jeff Harris <jeff@jeffharris.us>
2020-06-03 12:31:32 -0700

    Add image to buttons.

--- resources/views/workorders/edit.blade.php
+++ resources/views/workorders/edit.blade.php
@@ -115,12 +115,12 @@
               class="btn btn-outline-primary col-4 offset-1"
               id="commitChangesButton"
               type="button">
-              Commit Changes
+              <i class="fas fa-save"></i>&nbsp;Commit Changes
             </button>
             <button
               class="btn btn-outline-secondary col-4 offset-2"
               type="reset">
-              Revert Changes
+              <i class="fas fa-undo-alt"></i>&nbsp;Revert Changes
             </button>
           </div>
           <div id='alert_row' class="row">
@@ -166,7 +166,7 @@
               data-toggle="modal"
               id="addInventoryItemButton"
               type="button"
-            >Add Inventory Item
+            ><i class="fas fa-pallet"></i>&nbsp;Add Inventory Item
             </button>
             <button
               class="btn btn-sm col-1 offset-4"
@@ -244,7 +244,16 @@
                 id="productSubmit"
                 type="button"
                 disabled
-              >Add Product
+              >
+                <svg class="bi bi-bag-plus" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor"
+                     xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd"
+                        d="M14 5H2v9a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V5zM1 4v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4H1z"/>
+                  <path d="M8 1.5A2.5 2.5 0 0 0 5.5 4h-1a3.5 3.5 0 1 1 7 0h-1A2.5 2.5 0 0 0 8 1.5z"/>
+                  <path fill-rule="evenodd"
+                        d="M8 7.5a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"/>
+                  <path fill-rule="evenodd" d="M7.5 10a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-2z"/>
+                </svg>&nbsp;Add Product
               </button>
             </div>
             <div class="row" id="productError"></div>
