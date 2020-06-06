13206a4bf008d15f4643f14422bbc8ca3310c234
Jeff Harris <jeff@jeffharris.us>
2020-06-05 22:54:40 -0700

    Took App language out of Domain side.
    App language combined and moved into WorkOrderResource.php

--- app/Domain/WorkOrders/Actions/WorkOrdersUpdateAction.php
+++ app/Domain/WorkOrders/Actions/WorkOrdersUpdateAction.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Copyright 2018, 2019 Jeff Harris
+ * Copyright 2018, 2019, 2020 Jeff Harris
  * PHP Version 7.4
  */
 
@@ -12,7 +12,8 @@ namespace Domain\WorkOrders\Actions;
 use App\WorkOrders\DataTransferObjects\ClientObject;
 use App\WorkOrders\DataTransferObjects\PersonObject;
 use App\WorkOrders\DataTransferObjects\WorkOrderObject;
-use App\WorkOrders\Resources\WorkOrderResource;
+use Domain\WorkOrders\Models\Client;
+use Domain\WorkOrders\Models\Person;
 use Domain\WorkOrders\Models\WorkOrder;
 
 /**
@@ -36,48 +37,45 @@ class WorkOrdersUpdateAction
         ClientObject $clientObject,
         PersonObject $personObject
     ): array {
-        $changedFields[WorkOrder::ID] = $workOrder->luhn;
-
+        $changedFields[WorkOrder::ID] = $workOrder->id;
+        $changedFields[WorkOrder::LUHN] = $workOrder->luhn;
         $client = $workOrder->client;
         $person = $client->person;
 
-        // Override only what is set. Keep original data. Keep track of changes
+        // Override only what is in $wOO. Keep original data. Keep track of changes
         if (isset($workOrderObject->is_locked)) {
             $workOrder->is_locked = $workOrderObject->is_locked;
-            $changedFields[WorkOrderResource::IS_LOCKED] = $workOrderObject->is_locked;
+            $changedFields[WorkOrder::IS_LOCKED] = $workOrderObject->is_locked;
         }
         if (isset($workOrderObject->intake)) {
             $workOrder->intake = $workOrderObject->intake;
-            $changedFields[WorkOrderResource::INTAKE] = $workOrderObject->intake;
+            $changedFields[WorkOrder::INTAKE] = $workOrderObject->intake;
         }
         if (isset($clientObject->company_name)) {
             $client->company_name = $clientObject->company_name;
-            $changedFields[WorkOrderResource::CLIENT_COMPANY_NAME] = $clientObject->company_name;
+            $changedFields[Client::COMPANY_NAME] = $clientObject->company_name;
         }
         if (isset($personObject->first_name)) {
             $person->first_name = $personObject->first_name;
-            $changedFields[WorkOrderResource::FIRST_NAME] = $personObject->first_name;
+            $changedFields[Person::FIRST_NAME] = $personObject->first_name;
         }
         if (isset($personObject->last_name)) {
             $person->last_name = $personObject->last_name;
-            $changedFields[WorkOrderResource::LAST_NAME] = $personObject->last_name;
+            $changedFields[Person::LAST_NAME] = $personObject->last_name;
         }
         if (isset($personObject->phone_number)) {
             $person->phone_number = $personObject->phone_number;
-            $changedFields[WorkOrderResource::PHONE_NUMBER] = $personObject->phone_number;
+            $changedFields[Person::PHONE_NUMBER] = $personObject->phone_number;
         }
         if (isset($personObject->email)) {
             $person->email = $personObject->email;
-            $changedFields[WorkOrderResource::EMAIL] = $personObject->email;
+            $changedFields[Person::EMAIL] = $personObject->email;
         }
 
         $client->person()->save($person);
         $client->save();
         $workOrder->client()->associate($client);
         $workOrder->save();
-        if (!empty($changedFields)) {
-            $changedFields['luhn'] = $workOrder->luhn;
-        }
 
         return $changedFields;
     }
